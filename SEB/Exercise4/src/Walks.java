e044a1a4cf30b4dd9017f7ab1b8639228827794f
Simon Stratemeier <sstratem@stud.hs-heilbronn.de>
2018-11-02 15:55:41 +0100

    Add Exercise 4

---
+++ SEB/Exercise4/src/Walks.java
@@ -0,0 +1,134 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * @author Simon Stratemeier 199067
+ */
+public class Walks {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Bitte n angeben:");
+    int n = scanner.nextInt();
+    PathCounter pathCounter  = new PathCounter(n);
+   System.out.println("Count paths: " + pathCounter.countPaths());
+    System.out.println("Count points visited: " + pathCounter.countPoints());
+  }
+}
+
+class PathCounter {
+  private int n;
+  private Paths[][][] cache;
+
+  public BigInteger countPaths() {
+    return countPaths(0, 0, Vector.ZERO).getCountPaths();
+  }
+
+  public BigInteger countPoints() {
+    return countPaths(0, 0, Vector.ZERO).getCountPoints();
+  }
+
+  private Paths countPaths(int x, int y, Vector src) {
+    if (outOfBoundaries(x, y)) {
+      return Paths.ZERO;
+    } else if (targetReached(x, y)) {
+      // Don´t return new Paths(BigInteger.ONE, steps) -> Wrong result
+      // instead return Paths.ONE <- Credits to Tobias Fetzer 198318
+      return Paths.ONE;
+    } else if (isCached(x, y, src)) {
+      return cache[x][y][src.getCacheId()];
+    } else {
+      Paths paths = Paths.ZERO;
+      for (Vector vec : src.getNextVectors()) {
+        paths = paths.add(countPaths(vec.x(x), vec.y(y), vec));
+      }
+      cache[x][y][src.getCacheId()] = paths;
+      return paths;
+    }
+  }
+
+  private boolean isCached(int x, int y, Vector src) {
+    return  cache[x][y][src.getCacheId()] != null;
+  }
+
+  private boolean outOfBoundaries(int x, int y) {
+    return  x < 0 || y < 0 || y > n - x;
+  }
+
+  private boolean targetReached(int x, int y) {
+    return x == n && y == 0;
+  }
+
+  public PathCounter(int n) {
+    this.n = n;
+    cache = new Paths[n+1][n+1][5];
+  }
+}
+
+class Paths {
+  private BigInteger countPaths;
+  private BigInteger countPoints;
+
+  public BigInteger getCountPoints() {
+    return countPoints;
+  }
+
+  public BigInteger getCountPaths() {
+    return countPaths;
+  }
+
+  public Paths(BigInteger countPaths, BigInteger countPoints) {
+    this.countPaths = countPaths;
+    this.countPoints = countPoints;
+  }
+
+  public Paths add(Paths paths) {
+    return new Paths(
+        getCountPaths().add(paths.getCountPaths()),
+        getCountPoints().add(paths.getCountPaths().add(paths.getCountPoints()))
+        );
+  }
+
+  public static final Paths ZERO = new Paths(BigInteger.ZERO, BigInteger.ZERO);
+  public static final Paths ONE = new Paths(BigInteger.ONE, BigInteger.ONE);
+}
+
+enum Vector {
+  RIGHT(0, 1, 0), UP(1, 0, 1), LEFTUP(2, -1, 1), RIGHTDOWN(3, 1, -1), RIGHTUP(0, 1, 1), ZERO(4, 0, 0);
+
+  private int dx;
+  private int dy;
+  private int cacheId;
+  private Vector[] nextVectors;
+
+  public int x (int srcX) {
+    return srcX + dx;
+  }
+  public int y (int srcY) {
+    return srcY + dy;
+  }
+  public int getCacheId() {
+    return cacheId;
+  }
+  public void setNextVectors(Vector[] nextVectors) {
+    this.nextVectors = nextVectors;
+  }
+  public Vector[] getNextVectors() {
+    return nextVectors;
+  }
+
+  static {
+    Vector.RIGHT.setNextVectors(new Vector[] {Vector.RIGHT, Vector.LEFTUP, Vector.RIGHTDOWN, Vector.RIGHTUP});
+    Vector.UP.setNextVectors(new Vector[] {Vector.UP, Vector.LEFTUP, Vector.RIGHTDOWN});
+    Vector.LEFTUP.setNextVectors(new Vector[] {Vector.RIGHT, Vector.UP, Vector.LEFTUP, Vector.RIGHTUP});
+    Vector.RIGHTDOWN.setNextVectors(new Vector[] {Vector.RIGHT, Vector.UP, Vector.RIGHTDOWN, Vector.RIGHTUP});
+    Vector.RIGHTUP.setNextVectors(new Vector[] {Vector.RIGHT, Vector.LEFTUP, Vector.RIGHTDOWN, Vector.RIGHTUP});
+    Vector.ZERO.setNextVectors(new Vector[] {Vector.RIGHT, Vector.UP, Vector.LEFTUP, Vector.RIGHTDOWN, Vector.RIGHTUP});
+  }
+
+  Vector(int id, int dx, int dy) {
+    this.cacheId = id;
+    this.dx = dx;
+    this.dy = dy;
+  }
+}
